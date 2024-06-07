30eac9f406d75683da9fa23cdf2bb18de2345ec7
PokemonMasterSMC <279979091@qq.com>
2024-06-07 21:13:03 +0200

    fix  url parser

--- consumer/app/message_queue.py
+++ consumer/app/message_queue.py
@@ -3,6 +3,8 @@ from worker import process_chunk
 import json
 import threading
 import logging
+from urllib.parse import unquote
+
 
 
 
@@ -102,8 +104,11 @@ def initialize_subscriber():
             try:
                 data = json.loads(message.data.decode('utf-8'))
                 logging.info(f"Received message : {data}")
+                
+                # Decode the URL
+                video_url = unquote(data['video_url'])
 
-                process_chunk(data['job_id'], data['video_url'], data['watermark_path'], data['start'], data['end'], data['chunk_num'], data['total_chunks'])
+                process_chunk(data['job_id'], video_url, data['watermark_path'], data['start'], data['end'], data['chunk_num'], data['total_chunks'])
                 message.ack()
             except Exception as e:
                 logging.info(f"An error occurred while processing message: {e}")
