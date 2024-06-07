dd994432366de94ff920f731b4067d35b651fd0c
PokemonMasterSMC <279979091@qq.com>
2024-06-07 18:02:48 +0200

    feat  consumer init

---
+++ consumer/app/message_queue1.py
@@ -0,0 +1,116 @@
+from google.cloud import pubsub_v1
+from worker2 import process_chunk
+import json
+import threading
+import logging
+
+
+
+project_id = "watermarking-424614"
+topic_id = "image-watermark"
+
+pub_client = pubsub_v1.PublisherClient()
+topic_path = pub_client.topic_path(project_id, topic_id)
+
+
+topic_name = 'projects/{project_id}/topics/{topic}'.format(
+    project_id=project_id,
+    topic=topic_id,  # Set this to something appropriate.
+)
+
+
+"""
+
+def initialize_publisher():
+    publisher = pubsub_v1.PublisherClient()
+    topic_path = publisher.topic_path(project_id, topic_id)
+
+    def publish_messages():
+        for i in range(10):
+            data = f"Message number {i}"
+            # Data must be a bytestring
+            data = data.encode("utf-8")
+            # Publish a message
+            future = publisher.publish(topic_path, data)
+            print(f"Published {data} to {topic_path}: {future.result()}")
+            """
+
+    #从某个视频的第start帧操作到第end帧
+    # def publish_messages(video_url, start, end):
+    #     if video_url and start and end:
+    #         # Create a map object
+    #         data = {
+    #             "video_url": video_url,
+    #             "start": start,
+    #             "end": end
+    #         }
+    #         # Convert the map object to a JSON string
+    #         data = json.dumps(data)
+    #         # Data must be a bytestring
+    #         data = data.encode("utf-8")
+    #         # Publish a message
+    #         future = publisher.publish(topic_path, data)
+    #         print(f"Published {data} to {topic_path}: {future.result()}")
+
+def publish_messages(job_id, video_path, watermark_path, chunks):
+        logging.info(f"Topic path: {topic_path}")
+        for i, (start, end) in enumerate(chunks):
+            data = json.dumps({
+                'job_id': job_id,
+                'video_url': video_path,
+                'watermark_path': watermark_path,
+                'start': start,
+                'end': end,
+                'chunk_num': i + 1,
+                'total_chunks': len(chunks)
+            }).encode('utf-8')
+            future = pub_client.publish(topic_name, data)
+            logging.info(f"Published message {i + 1} to {topic_name}")
+            logging.info.info(f"Published message future: {future.result()}")
+
+    #publish_messages()
+
+
+
+
+
+subscription_id = 'image--watermark-sub1'
+
+subscription_name = 'projects/{project_id}/subscriptions/{sub}'.format(
+    project_id=project_id,
+    sub=subscription_id,  # Set this to something appropriate.
+)
+
+
+def initialize_subscriber():
+        #current_app.logger.info(f"Subscription name: {subscription_name}")
+        subscriber = pubsub_v1.SubscriberClient()
+        subscription_path = subscriber.subscription_path(project_id, subscription_id)
+
+    
+
+    # def callback(message):
+    #     # Decode the bytestring to a JSON string
+    #     json_str = message.data.decode('utf-8')
+    #     # Parse the JSON string to a map object
+    #     data = json.loads(json_str)
+    #     print(f"Received message: {data}")
+    #     message.ack()
+
+    
+        def callback(message): #处理来自pub的消息
+            try:
+                data = json.loads(message.data.decode('utf-8'))
+                logging.info(f"Received message: {data}")
+
+                process_chunk(data['job_id'], data['video_url'], data['watermark_path'], data['start'], data['end'])
+                message.ack()
+            except Exception as e:
+                print(f"An error occurred while processing message: {e}")
+                message.nack()
+
+        streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
+        logging.info(f"Listening for messages on {subscription_path}\n")
+        logging.info(f"streaming_pull_future: {streaming_pull_future}")
+
+
